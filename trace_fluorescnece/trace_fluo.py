6dea4994e2ebfbe58f9b11ab111c1675fe9170e4
hengjiwang <hengjw@uw.edu>
2019-10-11 15:40:25 -0700

    Store intensity rather than frames

--- trace_fluorescnece/trace_fluo.py
+++ trace_fluorescnece/trace_fluo.py
@@ -3,21 +3,27 @@ import matplotlib.pyplot as plt
 import cv2
 import sys, os
 
-def load_video(video, display=True):
+def trace(video, display=True):
     '''
-    :return: an array of frames_ from the video
-    :rtype: numpy.array
+    :return: an array of the integration of fluorescence for each frame of the video
+    :rtype: list
     '''
 
-    frames_ = []
+    intensities_ = []
     cap = cv2.VideoCapture(video)
+    firstf = True
     while True:
         # Capture frame-by-frame
         ret, frame = cap.read()
         if ret:
+            if firstf:
+                size = len(frame) * len(frame[0])
+                firstf = False
             # Our operations on the frame come here
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            frames_.append(frame)
+            intensity = np.sum(frame)
+
+            intensities_.append(intensity/size)
 
             # Display the resulting frame
             if display:
@@ -35,22 +41,12 @@ def load_video(video, display=True):
     cap.release()
     cv2.destroyAllWindows()
 
-    frames_ = np.array(frames_)
-
-    return frames_
-
-def trace(frames_):
-    '''
-    :return: integrate the fluorescence intensities in the frames
-    :rtype: numpy.array
-    '''
-    intensity = np.sum(np.sum(frames, axis = 1), axis = 1)
-    return intensity
+    return intensities_
 
 def plot(fluorescence_, fps=20, save=True, filename=None):
     fig = plt.figure()
     x = np.linspace(0, len(fluorescence_)/fps, len(fluorescence_))
-    plt.plot(x, fluorescence, 'r-')
+    plt.plot(x, fluorescence_, 'r-')
     plt.xlabel('time/s')
     plt.ylabel('fluorescence')
     plt.show()
@@ -66,12 +62,12 @@ def plot(fluorescence_, fps=20, save=True, filename=None):
         except FileNotFoundError:
             os.makedirs('data/')
             np.savetxt('data/' + filename, fluorescence_)
+    return
 
 
 if __name__ == "__main__":
     videoname = sys.argv[1] # 'videos/NGCaMP6_50X_20fps_M1-004.avi'
-    frames = load_video(videoname, False)
-    fluorescence = trace(frames)
+    intensities = trace(videoname, True)
     filename = videoname.split('/')[-1].strip('.avi')
-    plot(fluorescence, int(sys.argv[2]), True, filename)
+    plot(intensities, int(sys.argv[2]), True, filename)
     
