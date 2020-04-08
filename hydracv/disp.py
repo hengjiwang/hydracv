f1110184a933d818028848239b2aa276085255f3
hengjiwang <hengjw@uw.edu>
2020-04-07 20:48:31 -0700

    Colored different videos

--- hydracv/disp.py
+++ hydracv/disp.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 def add_fluorescence(ax, time_axis, fluo):
     """Add a plot of fluo to ax"""
@@ -26,6 +27,7 @@ def add_spike_trains(ax, spk_trains, fps):
     
     ax.set_xlabel("time(s)")
     ax.set_ylabel("cluster #")
+    ax.set_ylim(0, len(spk_trains))
 
 def add_isi_curves(ax, isi):
     """Add ISI curves to ax"""
@@ -39,3 +41,13 @@ def add_boxplot(ax, isi_t):
     ax.boxplot(isi_t)
     ax.set_xlabel("Interval #")
     ax.set_ylabel("ISI(s)")
+
+def random_color():
+    return "#{0:02x}{1:02x}{2:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+def add_color_stripes(ax, videos, total_num):
+    """Add color stripes on the spiking trains plot"""
+    count = 0
+    for video in videos.values():
+        ax.axvspan(xmin=0, xmax=70, ymin=count/total_num, ymax=(count+video.num_pk_clstrs())/total_num, alpha=0.1, color=random_color())
+        count += video.num_pk_clstrs()
